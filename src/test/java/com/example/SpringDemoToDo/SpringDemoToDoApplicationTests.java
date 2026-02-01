ee70c791e4943e28a0abaa2719ee2130f47524b4
Alok Prakash <alokprakash@Aloks-MacBook-Air.local>
2026-02-01 17:41:19 +0530

    Initial commit - SpringDemoToDo project

---
+++ src/test/java/com/example/SpringDemoToDo/SpringDemoToDoApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.example.SpringDemoToDo;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class SpringDemoToDoApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
